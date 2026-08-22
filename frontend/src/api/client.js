8acd3f762cee15bce5e8a5e7f52d9c8da1759b97
adriandlg-m <adriandlg1604@gmail.com>
2026-08-22 15:29:02 -0400

    update: frontend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8082/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: import.meta.env.VITE_API_URL || "/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
 );
 
 export default apiClient;
